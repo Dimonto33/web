970dd89c1d42a96482f5b224e348b163e3bfe4c2
Alejandro <alejandrouribep@gmail.com>
2019-04-03 15:39:32 -0500

    Update server.js

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ router.get('/dinero',(request, response) =>{
 
 app.use("/", router);
 
-app.listen(8080, () =>{
-  console.log("server is now runing in port 8080")
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
